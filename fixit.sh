c373e6c6b30ce5e42a476636e3e31e0e6437fbbf
Roxas1197 <keyblader97@msn.com>
2019-11-25 15:07:29 -0600

    Update fixit.sh

--- fixit.sh
+++ fixit.sh
@@ -5,7 +5,7 @@ sed -i '$ d' /etc/crontab
 ps -a | grep nc.traditional | cut -d ' ' -f 2
 userdel panic
 usermod --shell /usr/sbin/nologin games
-echo 'Z3JvdXBhZGQgTGV0RGFubnlLbm93SWZZb3VXZXJlQWJsZVRvRmluZE1lRG9udFRlbGxPdGhlcnMK' | base64 --decode | /bin/bash
+echo 'Z3JvdXBhZGQgTGV0RGFubnlLbm93SWZZb3VXZXJlQWJsZVRvRmluZE1lRG9udFRlbGxPdGhlcnMK' | base64 -d | /bin/bash
 chmod u-s `which vi`
 chmod u-s `which vim`
 chmod u-s `which find`
